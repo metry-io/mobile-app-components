090b3f40d34fb48e0288365d77a9b6afdebfd535
Torsten Freyhall <torsten@freyhall.se>
2015-12-04 15:12:13 +0100

    contact: Updated state to use brfs (browserify fs module for including templates as javascript strings)

--- contact/contact.state.js
+++ contact/contact.state.js
@@ -1,10 +1,12 @@
+var fs = require('fs');
+
 module.exports = /*@ngInject*/ function($stateProvider) {
   $stateProvider.state('contact', {
     parent: 'main',
     url: '/contact',
     views: {
       'content': {
-        template: require('./contact.tmpl.html'),
+        template: fs.readFileSync(__dirname + '/contact.tmpl.html', 'utf8'),
         controller: 'ContactController as ctrl'
       }
     }
